36bd3b3e047151e87fdd82a884e76d8429e284a0
JihyeKimBio <kim2050@indiana.edu>
2019-11-03 21:05:26 -0600

    Add files via upload

--- challenge_sec3_plot1.py
+++ challenge_sec3_plot1.py
@@ -4,7 +4,6 @@
 
 # Projections of Total Numbers of Americans Age 65 and Older with Alzheimer's Dementia by State
 
-
 import pandas as pd
 import numpy as np
 import math
@@ -17,11 +16,13 @@ Alz_num_2018 = pd.read_csv('Alzheimer_2018_statistics_1.csv')
 x=Alz_num_2018['State']
 y1=Alz_num_2018['2018']
 y2=Alz_num_2018['2025']
-plt.plot(x,y1, color='pink', marker='o')
+plt.figure(figsize=(15,5))
+plt.plot(x,y1, color='blue', marker='o')
 plt.plot(x,y2, color='gray', marker='v')
-plt.title('Projections of Total Numbers of Americans Age 65 and Older with Alzheimer\'s Dementia by State')
-plt.xlabel('State')
-plt.ylabel('Projected Number with Alzheimer\'s (in thousands')
+plt.title('Americans(Age>65) with Alzheimer by State', fontsize=25)
+plt.xlabel('State', fontsize=18)
+plt.ylabel('Number (in thousands)', fontsize=18)
 plt.show()
-plt.savefig('section3_figure1.png')
+plt.savefig('section3_plot1.png')
+
 
